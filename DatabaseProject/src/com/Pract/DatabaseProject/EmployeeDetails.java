b3c4567cc2d329cf1651d69fd55c7ee06ccf7ec2
sandeepkorrapati <sandeep.korrapati111@gmail.com>
2018-08-17 14:27:14 -0500

    Added Employee POJO that has employee id, name, salary, and numberOfWorkingDays fields

---
+++ DatabaseProject/src/com/Pract/DatabaseProject/EmployeeDetails.java
@@ -0,0 +1,78 @@
+package com.Pract.DatabaseProject;
+
+public class EmployeeDetails {
+	private Long id;
+	private String name;
+	private double salary;
+	private Long numOfWorkingDays;
+	public EmployeeDetails() {
+	
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Long getNumOfWorkingDays() {
+		return numOfWorkingDays;
+	}
+	public void setNumOfWorkingDays(Long numOfWorkingDays) {
+		this.numOfWorkingDays = numOfWorkingDays;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((numOfWorkingDays == null) ? 0 : numOfWorkingDays.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmployeeDetails other = (EmployeeDetails) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (numOfWorkingDays == null) {
+			if (other.numOfWorkingDays != null)
+				return false;
+		} else if (!numOfWorkingDays.equals(other.numOfWorkingDays))
+			return false;
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
